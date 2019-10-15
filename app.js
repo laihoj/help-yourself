efc09d9b83364189c6c2e8314a2a8ce452b668b0
Jaakko Laiho <jaakkolaiho94@gmail.com>
2019-10-15 14:11:51 +0300

    small tweak

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-require('dotenv').config();
+
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 var bodyParser 				= require("body-parser"),
 	express 				= require("express"),
