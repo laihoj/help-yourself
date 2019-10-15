9fcbe3639337f7f8b0fb36e798a9a69cf7cb8789
Jaakko Laiho <jaakkolaiho94@gmail.com>
2019-10-15 12:01:18 +0300

    typo fix

--- db.js
+++ db.js
@@ -3,54 +3,14 @@ Mongoose.js and MongoDB
 ********************************************************/
 const mongoose = require('mongoose')
 
-var url = process.env.DATABASEURL || "mongodb://user:monday1@ds235078.mlab.com:35078/helpme";
+var url = process.env.DATABASEURL;
 mongoose.connect(url, { useNewUrlParser: true });
 
 const User = require("./models/user");
-// const Device = require("./models/device");
-// const Datapoint = require("./models/datapoint");
-// const Gesture = require("./models/gesture");
 const Category = require("./models/category");
 const Item = require("./models/item");
 const Effort = require("./models/effort");
 const Relevance = require("./models/relevance");
-
-
-// exports.datapoints = async function(latest_N) {
-// 	if(arguments.length == 0) {
-// 		return Datapoint.find({}).exec();
-// 	}
-// 	//not used anywhere?
-// 	if(arguments.length == 1) {
-// 		return Datapoint.find({})
-// 						.skip(db.collection.count() - N)
-// 						.exec();
-// 	}
-// }
-
-
-// exports.datapointsInTimeSpan = async function(start, end) {
-// 	return Datapoint.find({
-// 		timestamp:{$gt: start, $lt: end}}).exec();
-// }
-
-// exports.saveDatapoint = async function(device_address, timestamp, data) {
-// 	var datapoint = new Datapoint({
-// 		device_address: device_address,
-// 		timestamp: timestamp,
-// 		data: data
-// 	});
-//  	return datapoint.save();
-// }
-
-// exports.devices = async function() {
-// 	return Device.find({}).exec();
-// }
-
-// exports.gestures = async function() {
-// 	return Gesture.find({}).exec();
-// }
-
 
 exports.categories = async function() {
 	return Category.find({}).exec();
@@ -69,18 +29,6 @@ exports.relevances = async function() {
 }
 
 
-// exports.getGesturesByLabel = async function(label) {
-// 	return Gesture.find({label: label}).exec();
-// }
-
-// exports.getDeviceByAddress = async function(address) {
-// 	return Device.findOne({address: address}).exec();
-// }
-
-// exports.getDevicesByUser = async function(user) {
-// 	return Device.find({user: user}).exec();
-// }
-
 exports.getCategoriesByUser = async function(user) {
 	return Category.find({user: user}).exec();
 }
@@ -96,28 +44,6 @@ exports.getEffortsByUser = async function(user) {
 exports.getRelevancesByUser = async function(user) {
 	return Relevance.find({user: user}).exec();
 }
-
-
-// exports.saveDevice = async function(device_label, device_address, device_uuid, device_name, device_user) {
-// 	var device = new Device({
-// 		label: device_label,
-// 		address: device_address,
-// 		uuid: device_uuid,
-// 		name: device_name,
-// 		user: device_user
-// 	});
-//  	return device.save();
-// }
-
-// exports.saveGesture = async function(start_timestamp, end_timestamp, label, user) {
-// 	var gesture = new Gesture({
-// 		start_timestamp: start_timestamp,
-// 		end_timestamp: end_timestamp,
-// 		label: label,
-// 		user: user
-// 	});
-//  	return gesture.save();
-// }
 
 
 
